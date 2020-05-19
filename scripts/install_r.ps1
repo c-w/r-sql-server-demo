f5ffabc79c85f74df1ff8dd10564bb58d62d370e
Clemens Wolff <clewolff@microsoft.com>
2020-05-19 16:27:53 -0400

    Downgrade to R version 3.5

--- scripts/install_r.ps1
+++ scripts/install_r.ps1
@@ -5,7 +5,7 @@ if ([bool](Get-Command Rscript -ErrorAction SilentlyContinue))
 
 if ([bool](Get-Command choco -ErrorAction SilentlyContinue))
 {
-  $version = '4.0.0'
+  $version = '3.5.3'
   choco install R.Project --version=$version
   $rPath="$env:SystemDrive\Program Files\R\R-$version\bin"
   $env:PATH="$rPath;$env:PATH"
@@ -14,7 +14,7 @@ if ([bool](Get-Command choco -ErrorAction SilentlyContinue))
 elseif ([bool](Get-Command apt-get -ErrorAction SilentlyContinue))
 {
   $release = lsb_release --codename --short
-  $version = '40'
+  $version = '35'
   sudo apt-key adv --keyserver 'keyserver.ubuntu.com' --recv-keys 'E298A3A825C0D65DFD57CBB651716619E084DAB9'
   sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $release-cran$version/"
   sudo apt-get update
