dc91d9ee1ac167ccd76a2f905c518121c39a2b0a
Clemens Wolff <clewolff@microsoft.com>
2020-05-20 10:42:49 -0400

    Ensure R Tools is installed

--- scripts/install_r.ps1
+++ scripts/install_r.ps1
@@ -5,18 +5,28 @@ if ([bool](Get-Command Rscript -ErrorAction SilentlyContinue))
 
 if ([bool](Get-Command choco -ErrorAction SilentlyContinue))
 {
-  $version = '3.5.3'
-  choco install R.Project --version=$version
-  $rPath="$env:SystemDrive\Program Files\R\R-$version\bin"
-  $env:PATH="$rPath;$env:PATH"
-  Write-Host "##vso[task.prependpath]$rPath"
+  $rVersion = '3.5.3'
+  $rtoolsVersion = '3.5.0.4'
+  $arch = if ([Environment]::Is64BitProcess) { "64" } else { "32" }
+
+  choco install R.Project --version=$rVersion
+  choco install -y rtools --version=$rtoolsVersion
+
+  $rPath = "$env:SystemDrive\Program Files\R\R-$rVersion\bin"
+  $rtoolsPath = "$env:SystemDrive\Rtools\bin"
+  $mingwPath = "$env:SystemDrive\Rtools\mingw_$arch\bin"
+  $path = "$rPath;$rtoolsPath;$mingwPath"
+
+  $env:PATH = "$path;$env:PATH"
+  Write-Host "##vso[task.prependpath]$path"
 }
 elseif ([bool](Get-Command apt-get -ErrorAction SilentlyContinue))
 {
   $release = lsb_release --codename --short
-  $version = '35'
+  $rVersion = '35'
+
   sudo apt-key adv --keyserver 'keyserver.ubuntu.com' --recv-keys 'E298A3A825C0D65DFD57CBB651716619E084DAB9'
-  sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $release-cran$version/"
+  sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $release-cran$rVersion/"
   sudo apt-get update
   sudo apt-get install -y r-base
 }
